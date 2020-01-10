c76cdb2755928bffbeae0007e0ba0be60dabd94c
SIDDHARTH KAUSHIK <siddharth9k9@gmail.com>
2020-01-10 07:04:36 -0800

    blog

--- src/context/createDataContext.js
+++ src/context/createDataContext.js
@@ -1,15 +1,15 @@
 import React ,{useReducer} from 'react';
-
+import {Text} from 'react-native';
 
 export default (reducer,actions,initialState)=> {
     const Context = React.createContext();
-    const Provider = (c)=>{
+    const Provider = ({childern})=>{
         const [state,dispatch] = useReducer(reducer,initialState);
         const boundAction = {};
         for (let key in actions){
             boundAction[key] = actions[key][dispatch];
         }
-        return <Context.Provider value={{state,...boundAction}}>{c.childern}</Context.Provider>;
+        return <Context.Provider value={{state,...boundAction}}>{childern}</Context.Provider>;
     };
     return {Context,Provider};
 };
